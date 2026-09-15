3595d498ba13e50c52b2496b6fe7b4f220693321
jayproinfratech123 <jayproinfratech@gmail.com>
2026-09-15 18:12:44 +0530

    Use port 3000 as backend fallback

--- backend/server.js
+++ backend/server.js
@@ -191,7 +191,7 @@ app.use((error, req, res, next) => {
 // SERVER
 // ----------------------------------------------------
 
-const PORT = Number(process.env.PORT) || 5000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.listen(PORT, () => {
   console.log(
